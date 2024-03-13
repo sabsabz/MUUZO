043aa0f6cf03fa3c17af31c6807cc0292c486208
Kelvin Njiraini <njirainikevo46@gmail.com>
2024-03-13 23:09:36 +0300

    checkout.html changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,43 +1,48 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const User = require('../models/User');
+const User = require('../models/user');
 
 // Register a new user
 const register = async (req, res, next) => {
-  const { first_name, last_name, email, password } = req.body;
+    const { first_name, last_name, email, password } = req.body;
 
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ first_name, last_name, email, password: hashedPassword });
-    await user.save();
-    res.json({ message: 'Registration successful' });
-  } catch (error) {
-    next(error);
-  }
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({
+            first_name,
+            last_name,
+            email,
+            password: hashedPassword,
+        });
+        await user.save();
+        res.json({ message: 'Registration successful' });
+    } catch (error) {
+        next(error);
+    }
 };
 
 // Login with an existing user
 const login = async (req, res, next) => {
-  const { email, password } = req.body;
+    const { email, password } = req.body;
 
-  try {
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const passwordMatch = await user.comparePassword(password);
+        if (!passwordMatch) {
+            return res.status(401).json({ message: 'Incorrect password' });
+        }
+
+        const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+            expiresIn: '1 hour',
+        });
+        res.json({ token });
+    } catch (error) {
+        next(error);
     }
-
-    const passwordMatch = await user.comparePassword(password);
-    if (!passwordMatch) {
-      return res.status(401).json({ message: 'Incorrect password' });
-    }
-
-    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
-      expiresIn: '1 hour'
-    });
-    res.json({ token });
-  } catch (error) {
-    next(error);
-  }
 };
 
 module.exports = { register, login };
