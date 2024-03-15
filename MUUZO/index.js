7cfb8a17802eec2a5b3222040683cdd83a186882
SABBY SABZ <kasem@Shantae>
2024-03-15 12:25:18 +0300

    hey

---
+++ MUUZO/index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+const connectDB = require('./db');
+
+const authRoutes = require('./routes/user');
+const userRoutes = require('./routes/user');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+
+// Connect to MongoDB
+connectDB();
+
+// Parse JSON request body
+app.use(express.json());
+
+app.use(express.static('public'));
+
+// Define authentication routes
+app.use('/auth', authRoutes);
+
+// Define user routes
+app.use('/user', userRoutes);
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register', { includeScript: true });
+});
+
+app.get('/sign-in', (req, res) => {
+  res.render('sign-in', { includeScript: true });
+});
+
+app.get('/OTP', (req, res) => {
+  res.render('OTP', { includeScript: true });
+});
+
+app.get('/select-outlet', (req, res) => {
+  res.render('select-outlet', { includeScript: true });
+});
+
+app.get('/sales', (req, res) => {
+  res.render('sales', { includeScript: true });
+});
+
+app.get('/food', (req, res) => {
+  res.render('food', { includeScript: true });
+});
+
+app.get('/desert', (req, res) => {
+  res.render('desert', { includeScript: true });
+});
+
+app.get('/beverage', (req, res) => {
+  res.render('beverage', { includeScript: true });
+});
+
+app.get('/alcohol', (req, res) => {
+  res.render('alcohol', { includeScript: true });
+});
+
+app.get('/checkout', (req, res) => {
+  res.render('checkout', { includeScript: true });
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
---
+++ MUUZO/models/user.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+      unique: false
+    },
+    last_name: {
+      type: String,
+      required: true,
+      unique: false
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    phone_number:{
+      type: String,
+      required: true,
+      unique: false
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      enum: ['cashier', 'admin'],
+      default: 'cashier'
+    }
+  },
+  { timestamps: true }
+);
+
+// Hash the password before saving it to the database
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+
+  try {
+    const salt = await bcrypt.genSalt();
+    user.password = await bcrypt.hash(user.password, salt);
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+// Compare the given password with the hashed password in the database
+userSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
