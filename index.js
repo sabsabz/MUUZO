e9567a4e2e4a0667642151df5930df6becf08030
Kelvin Njiraini <njirainikevo46@gmail.com>
2024-03-26 09:29:40 +0300

    feat:customers page

--- index.js
+++ index.js
@@ -9,11 +9,9 @@ const productRoutes = require('./routes/product');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
-
 
 // Connect to MongoDB
 connectDB();
@@ -32,55 +30,58 @@ app.use('/user', userRoutes);
 app.use('/api/products', productRoutes);
 
 app.get('/', (req, res) => {
-  res.render('home');
+    res.render('home');
 });
 
 app.get('/about', (req, res) => {
-  res.render('about');
+    res.render('about');
 });
 
 app.get('/register', (req, res) => {
-  res.render('register', { includeScript: true });
+    res.render('register', { includeScript: true });
 });
 
-
 app.get('/sign-in', (req, res) => {
-  res.render('sign-in', { includeScript: true });
+    res.render('sign-in', { includeScript: true });
 });
 
 app.get('/OTP', (req, res) => {
-  res.render('otp', { includeScript: true });
+    res.render('otp', { includeScript: true });
 });
 
 app.get('/select-outlet', (req, res) => {
-  res.render('select-outlet', { includeScript: true });
+    res.render('select-outlet', { includeScript: true });
 });
 
 app.get('/food', (req, res) => {
-  res.render('food', { includeScript: true });
+    res.render('food', { includeScript: true });
 });
 
 app.get('/desert', (req, res) => {
-  res.render('desert', { includeScript: true });
+    res.render('desert', { includeScript: true });
 });
 
 app.get('/beverage', (req, res) => {
-  res.render('beverage', { includeScript: true });
+    res.render('beverage', { includeScript: true });
 });
 
 app.get('/alcohol', (req, res) => {
-  res.render('alcohol', { includeScript: true });
+    res.render('alcohol', { includeScript: true });
 });
 
 app.get('/sales', (req, res) => {
-  res.render('sales', { includeScript: true });
+    res.render('sales', { includeScript: true });
 });
 
 app.get('/checkout', (req, res) => {
-  res.render('checkout', { includeScript: true });
+    res.render('checkout', { includeScript: true });
+});
+
+app.get('/customers', (req, res) => {
+    res.render('customers', { includeScript: true });
 });
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
